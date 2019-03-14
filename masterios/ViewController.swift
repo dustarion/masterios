795f31d17c9ea4e337be9c77d2bf55a78f28aa4f
Qin Guan <qinguan20040914@gmail.com>
2019-03-14 14:37:23 +0800

    fuck

--- masterios/ViewController.swift
+++ masterios/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .black
+        self.view.backgroundColor = .gray
     }
 
 
---
+++ masterios/color.swift
@@ -0,0 +1,8 @@
+//
+// Created by Qin Guan on 2019-03-14.
+// Copyright (c) 2019 masterapp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
