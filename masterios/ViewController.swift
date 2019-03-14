6c10b988f8d044b9cf5e7044d8cf4cc3c5afd655
Qin Guan <qinguan20040914@gmail.com>
2019-03-14 20:11:59 +0800

    fuck

--- masterios/ViewController.swift
+++ masterios/ViewController.swift
@@ -14,7 +14,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(rgb: 0x212121)
+
+        loadViews()
+
     }
 
+    fileprivate func loadViews() {
+        let image = UIImageView(image: UIImage(named: "MasterIconWhite"))
+        self.view.addSubview(image)
+
+        image.translatesAutoresizingMaskIntoConstraints = false
+        let top = NSLayoutConstraint(item: image, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0)
+        NSLayoutConstraint.activate([top])
+    }
 
 }
