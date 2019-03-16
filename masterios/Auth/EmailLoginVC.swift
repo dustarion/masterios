02d2cba5087ce63ef6d8d9db069658ac2df35810
Qin Guan <qinguan20040914@gmail.com>
2019-03-16 12:28:44 +0800

    fuck

--- masterios/Auth/EmailLoginVC.swift
+++ masterios/Auth/EmailLoginVC.swift
@@ -11,19 +11,19 @@ import Hero
 import Alamofire
 
 class EmailLoginVC: UIViewController {
-    override func viewDidAppear(_ animated: Bool) {
-        //
-    }
-    
-    
-    @IBAction func BackButton(_ sender: Any) {
-        toLoginVC() // Transition Back
-    }
-    
-    
-    fileprivate func toLoginVC() {
-        let LoginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
-        LoginVC.hero.modalAnimationType = .slide(direction: .right)
-        self.hero.replaceViewController(with: LoginVC)
-    }
+  override func viewDidAppear(_ animated: Bool) {
+    //
+  }
+
+
+  @IBAction func BackButton(_ sender: Any) {
+    toLoginVC() // Transition Back
+  }
+
+
+  fileprivate func toLoginVC() {
+    let LoginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+    LoginVC.hero.modalAnimationType = .slide(direction: .right)
+    self.hero.replaceViewController(with: LoginVC)
+  }
 }
--- masterios/Auth/LoginVC.swift
+++ masterios/Auth/LoginVC.swift
@@ -8,45 +8,45 @@ import Hero
 import Alamofire
 
 class LoginVC: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        hero.isEnabled = true
-        setupViews()
-        
-    }
-    
-    fileprivate func setupViews() {
-        // self.view.backgroundColor = .white
-    }
-    
-    @IBAction func GoogleSignIn(_ sender: Any) {
-        // Google Sign In
-        print("Google")
-        
-    }
-    
-    @IBAction func FacebookSignIn(_ sender: Any) {
-        // Facebook Sign In
-        print("Facebook")
-        
-    }
-    
-    @IBAction func EmailSignIn(_ sender: Any) {
-        // Email Sign In
-        print("Email")
-        toEmailLoginVC()
-    }
-    
-    @IBAction func EmailSignUp(_ sender: Any) {
-    }
-    
-    //EmailLoginVC
-    
-    fileprivate func toEmailLoginVC() {
-        
-        let EmailLoginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "EmailLoginVC") as! EmailLoginVC
-        EmailLoginVC.hero.modalAnimationType = .slide(direction: .left)
-        self.hero.replaceViewController(with: EmailLoginVC)
-    }
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    hero.isEnabled = true
+    setupViews()
+
+  }
+
+  fileprivate func setupViews() {
+    // self.view.backgroundColor = .white
+  }
+
+  @IBAction func GoogleSignIn(_ sender: Any) {
+    // Google Sign In
+    print("Google")
+
+  }
+
+  @IBAction func FacebookSignIn(_ sender: Any) {
+    // Facebook Sign In
+    print("Facebook")
+
+  }
+
+  @IBAction func EmailSignIn(_ sender: Any) {
+    // Email Sign In
+    print("Email")
+    toEmailLoginVC()
+  }
+
+  @IBAction func EmailSignUp(_ sender: Any) {
+  }
+
+  //EmailLoginVC
+
+  fileprivate func toEmailLoginVC() {
+
+    let EmailLoginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "EmailLoginVC") as! EmailLoginVC
+    EmailLoginVC.hero.modalAnimationType = .slide(direction: .left)
+    self.hero.replaceViewController(with: EmailLoginVC)
+  }
+
 }
