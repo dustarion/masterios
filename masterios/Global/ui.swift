025c27e5bb15582a8a02f766fdaa42a7cbe996d2
Dalton Ng <dustarion@gmail.com>
2019-04-23 12:12:36 +0800

    Added Dark Background

--- masterios/Global/ui.swift
+++ masterios/Global/ui.swift
@@ -23,14 +23,8 @@ public func showNewSetTabOptions(sender: UIViewController) {
     let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
     alert.view.tintColor = yellowTintColour
     
-    
-    let subview = alert.view.subviews.first! as UIView
-    let alertContentView = subview.subviews.first! as UIView
-    alertContentView.backgroundColor = .black  //UIColor.blackColor()
-    alert.view.layer.cornerRadius = 15
-    
-    //.black//newSetCardBackgroundColour
-    //alert.preferredStyle = //UIActionSheetStyle.blackTranslucent
+    // Dark Background on Action Sheet
+    alert.blurStyle = .dark
     
     alert.addAction(UIAlertAction(title: "New Study Set", style: .default , handler:{ (UIAlertAction) in
         // Transition New Set
@@ -49,11 +43,69 @@ public func showNewSetTabOptions(sender: UIViewController) {
     sender.present(alert, animated: true, completion: {
         // Done
     })
+    
+}
+
+extension UIAlertController {
+    
+    private struct AssociatedKeys {
+        static var blurStyleKey = "UIAlertController.blurStyleKey"
+    }
+    
+    public var blurStyle: UIBlurEffect.Style {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.blurStyleKey) as? UIBlurEffect.Style ?? .extraLight
+        } set (style) {
+            objc_setAssociatedObject(self, &AssociatedKeys.blurStyleKey, style, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+        }
+    }
+    
+    public var cancelButtonColor: UIColor? {
+        return blurStyle == .dark ? UIColor(red: 28.0/255.0, green: 28.0/255.0, blue: 28.0/255.0, alpha: 1.0) : nil
+    }
+    
+    private var visualEffectView: UIVisualEffectView? {
+        if let presentationController = presentationController, presentationController.responds(to: Selector(("popoverView"))), let view = presentationController.value(forKey: "popoverView") as? UIView // We're on an iPad and visual effect view is in a different place.
+        {
+            return view.recursiveSubviews.compactMap({$0 as? UIVisualEffectView}).first
+        }
+        
+        return view.recursiveSubviews.compactMap({$0 as? UIVisualEffectView}).first
+    }
+    
+    private var cancelActionView: UIView? {
+        return view.recursiveSubviews.compactMap({
+            $0 as? UILabel}
+            ).first(where: {
+                $0.text == actions.first(where: { $0.style == .cancel })?.title
+            })?.superview?.superview
+    }
+    
+    public convenience init(title: String?, message: String?, preferredStyle: UIAlertController.Style, blurStyle: UIBlurEffect.Style) {
+        self.init(title: title, message: message, preferredStyle: preferredStyle)
+        self.blurStyle = blurStyle
+    }
+    
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+//        visualEffectView?.effect = UIBlurEffect(style: blurStyle)
+        visualEffectView?.effect = UIBlurEffect(style: .regular)
+        cancelActionView?.backgroundColor = .darkGray//cancelButtonColor
+    }
 }
 
 extension UIView {
     
-    // Extensions
+    var recursiveSubviews: [UIView] {
+        var subviews = self.subviews.compactMap({$0})
+        subviews.forEach { subviews.append(contentsOf: $0.recursiveSubviews) }
+        return subviews
+    }
+    
     func searchVisualEffectsSubview() -> UIVisualEffectView? {
         
         if let visualEffectView = self as? UIVisualEffectView {
---
+++ masterios/New Set/NewSetVC.swift
@@ -0,0 +1,9 @@
+//
+//  NewSetVC.swift
+//  masterios
+//
+//  Created by Dalton Prescott Ng on 23/4/19.
+//  Copyright © 2019 masterapp. All rights reserved.
+//
+
+import Foundation
